9bf2bf25f9124bf8fe5500e03cc068daaa8ccf73
Adithyas2003 <adathyas49@gmail.com>
2024-11-13 11:37:25 +0530

    demo updated

--- textileprj/textileapp/views.py
+++ textileprj/textileapp/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate,login,logout
+from django.contrib import messages
 
-# Create your views here.
+def e_shop_login(req):
+    if req.method=='POST':
+        uname=req.POST['uname']
+        password=req.POST['password']
+        data=authenticate(username=uname,password=password)
+        if data:
+            login(req,data)
+            return redirect(shop_home)
+        else:
+            messages.warning(req, "invalid password")
+            return redirect(e_shop_login)
+    else:
+        return render(req,'login.html')
+	
+def register(req):
+    return render(req,'register.html')
+
+def shop_home(req):
+    return render(req,'shop/home.html')
