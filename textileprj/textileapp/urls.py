ea2fe8a8d577d095578a7315dc50f3558a2e2f91
Adithyas2003 <adathyas49@gmail.com>
2024-11-20 11:39:01 +0530

    file added

--- textileprj/textileapp/urls.py
+++ textileprj/textileapp/urls.py
@@ -7,5 +7,9 @@ urlpatterns = [
     path('logout',views.e_shop_logout),
     path('addproduct',views.add_product),
     path('edit_product/<pid>',views.edit_product),
-    path('delete_product/<pid>',views.delete_product)
+    path('delete_product/<pid>',views.delete_product),
+
+
+    path('register',views.register),
+    path('user_home',views.user_home),
 ]
--- textileprj/textileapp/views.py
+++ textileprj/textileapp/views.py
@@ -3,18 +3,25 @@ from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
 from .models import *
 import os
+from django.contrib.auth.models import User
 
 def e_shop_login(req):
     if 'shop' in req.session:
         return redirect(shop_home)
+    if 'user' in req.session:
+        return redirect(user_home)
     if req.method=='POST':
         uname=req.POST['uname']
         password=req.POST['password']
         data=authenticate(username=uname,password=password)
         if data:
             login(req,data)
-            req.session['shop']=uname
-            return redirect(shop_home)
+            if data.is_superuser:
+                req.session['shop']=uname
+                return redirect(shop_home)
+            else:
+                req.session['user']=uname
+                return redirect(user_home)
         else:
             messages.warning(req, "invalid password")
             return redirect(e_shop_login)
@@ -79,3 +86,33 @@ def delete_product(req,pid):
     os.remove('media/'+file)
     data.delete()
     return redirect(shop_home)
+
+
+#-----------------------------------------user----------------------------------
+
+
+
+
+def register(req):
+    if req.method=='POST':
+        username=req.POST['uname']
+        email=req.POST['email']
+        password=req.POST['password']
+        
+        try:
+            data=User.objects.create_user(first_name=username,email=email,username=email,password=password)
+            data.save()
+        except:
+            messages.warning(req,"username already exist")
+            return redirect(register)
+        return redirect(e_shop_login)
+    else:
+        return render(req,'user/register.html')
+
+
+def user_home(req):
+    if 'user' in req.session:
+        data=Product.objects.all()
+        return render(req,'user/home.html',{'products':data})
+    else:
+        return redirect(e_shop_login)
