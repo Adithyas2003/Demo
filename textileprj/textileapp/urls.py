147876bb601d1b0cf06cc840e3fb4fc1facf3c49
Adithyas2003 <adathyas49@gmail.com>
2024-11-22 11:27:10 +0530

    demo added

--- textileprj/textileapp/urls.py
+++ textileprj/textileapp/urls.py
@@ -14,5 +14,9 @@ urlpatterns = [
     path('user_home',views.user_home),
     path('view_product/<pid>',views.view_product),
     path('add_to_cart/<pid>',views.add_to_cart),
-    path('view_cart',views.view_cart)
+    path('view_cart',views.view_cart),
+    path('qty_in/<cid>',views.qty_in),
+    path('qty_dec/<cid>',views.qty_dec),
+
+
 ]
